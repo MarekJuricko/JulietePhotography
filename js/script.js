f81bc3d95e7bf1e815523e081b96151b3e18445e
Marek Juričko <marek.juricko@azet.sk>
2022-05-18 19:58:56 +0200

    created script.js file, added listeners for responsive navbar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector("#toggle-button")
+const navLinks = document.querySelectorAll(".btn")
+
+toggleButton.addEventListener("click", () => {
+    navLinks.forEach(x => x.classList.toggle("active"))
+})
