990ccf46c95c0988ffd3227f2aee7eef8048d4bf
Marek Juričko <marek.juricko@azet.sk>
2022-10-26 21:21:14 +0200

    script.js - removing active navBar links on scroll

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ const navLinks = document.querySelectorAll(".btn")
 toggleButton.addEventListener("click", () => {
     navLinks.forEach(x => x.classList.toggle("active"))
 })
+
+window.addEventListener("scroll", () =>{
+    navLinks.forEach(x => x.classList.remove("active")) 
+})
